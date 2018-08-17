33b221830d38b6923d7ebfc1755e035118c43250
HEMANT KUMAR <contact2theamit@gmail.com>
2018-08-17 19:45:12 +0530

    9 layouts for 9 compartments

--- app/src/main/java/com/example/rudra/ojas_for_tte/Compartment.java
+++ app/src/main/java/com/example/rudra/ojas_for_tte/Compartment.java
@@ -17,4 +17,37 @@ public class Compartment extends AppCompatActivity {
         Intent intent=new Intent(Compartment.this,Compartment1.class);
         startActivity(intent);
     }
+    public void cmprt2(View view){
+        Intent intent=new Intent(Compartment.this,compartment2.class);
+        startActivity(intent);
+    }
+    public void cmprt3(View view){
+        Intent intent=new Intent(Compartment.this,compartment3.class);
+        startActivity(intent);
+    }
+    public void cmprt4(View view){
+        Intent intent=new Intent(Compartment.this,compartment4.class);
+        startActivity(intent);
+    }
+    public void cmprt5(View view){
+        Intent intent=new Intent(Compartment.this,compartment5.class);
+        startActivity(intent);
+    }
+    public void cmprt6(View view){
+        Intent intent=new Intent(Compartment.this,compartment6.class);
+        startActivity(intent);
+    }
+    public void cmprt7(View view){
+        Intent intent=new Intent(Compartment.this,compartment7.class);
+        startActivity(intent);
+    }
+    public void cmprt8(View view){
+        Intent intent=new Intent(Compartment.this,compartment8.class);
+        startActivity(intent);
+    }
+    public void cmprt9(View view){
+        Intent intent=new Intent(Compartment.this,compartment9.class);
+        startActivity(intent);
+    }
+
 }
---
+++ app/src/main/java/com/example/rudra/ojas_for_tte/compartment5.java
@@ -0,0 +1,13 @@
+package com.example.rudra.ojas_for_tte;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class compartment5 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_compartment5);
+    }
+}
